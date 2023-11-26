1573a4d002f658d389e31e25aa0978d07480b63a
trongthuy221096 <trantrongthuy221096@gmail.com>
2023-11-26 14:52:06 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1/tiki");
+        await mongoose.connect("mongodb+srv://trantrongthuy221096:hRCJCx9jinOCp4aJ@cluster0.cdyzod3.mongodb.net/?retryWrites=true&w=majority");
         console.log("connect ok");
     } catch (error) {
         console.log(error);
